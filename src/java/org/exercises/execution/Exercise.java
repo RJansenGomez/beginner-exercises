6431244b375a1f44b1cf1b2399693919a96e054b
Ramai Jansen <rjansengomez@gmail.com>
2021-07-10 02:11:35 +0200

    Blocks exercise

--- src/java/org/exercises/execution/Exercise.java
+++ src/java/org/exercises/execution/Exercise.java
@@ -8,11 +8,23 @@ public class Exercise {
     private static int executionOrder;
 
     static {
-        executionOrder++;
+        checkOrder(executionOrder, 4);
     }
 
     static {
         executionOrder++;
+    }
+
+    static {
+        checkOrder(executionOrder, 5);
+    }
+
+    static {
+        executionOrder++;
+    }
+
+    static {
+        checkOrder(executionOrder, 8);
     }
 
     static {
@@ -43,10 +55,20 @@ public class Exercise {
         checkOrder(executionOrder, 0);
     }
 
+    public static void main(String[] args) {
+        System.out.println("ORDER STATIC BLOCKS");
+        //Bonus remove the comment and execute if you know how this is achieved
+        checkOrder(executionOrder, 10);
+    }
+
     private static void checkOrder(int executionOrder, int expectedPlace) {
         if (executionOrder != expectedPlace) {
             throw new RuntimeException("Wrong order defined, expected " + expectedPlace + ", actual " + executionOrder);
         }
+    }
+
+    static {
+        checkOrder(executionOrder, 3);
     }
 
     static {
@@ -55,28 +77,6 @@ public class Exercise {
 
     static {
         checkOrder(executionOrder, 2);
-    }
-
-    static {
-        checkOrder(executionOrder, 3);
-    }
-
-    static {
-        checkOrder(executionOrder, 4);
-    }
-
-    static {
-        checkOrder(executionOrder, 5);
-    }
-
-    static {
-        checkOrder(executionOrder, 8);
-    }
-
-    public static void main(String[] args) {
-        System.out.println("ORDER STATIC BLOCKS");
-        //Bonus remove the comment and execute if you know how this is achieved
-        checkOrder(executionOrder, 10);
     }
 
     {
